1aba89415c5a95a00471a366202d351cd23faafd
Alex Johnson <ajohnson0012@gmail.com>
2016-08-24 19:28:19 -0400

    almost done

--- mirror_app/db/schema.rb
+++ mirror_app/db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160823175346) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "name"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.string   "remember_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 
 end
