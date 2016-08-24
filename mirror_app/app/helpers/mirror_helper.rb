758ea12835728ce8b70024fcfb4e8490f04e3b40
Alex Johnson <ajohnson0012@gmail.com>
2016-08-24 19:47:49 -0400

    almost done (with .gitignore)

--- mirror_app/app/helpers/mirror_helper.rb
+++ mirror_app/app/helpers/mirror_helper.rb
@@ -1,2 +1,8 @@
 module MirrorHelper
+  def getWeatherID
+    return "f94b7617ae1f162059f124c32e102e2b"
+  end
+  def getClientID
+    return "612433509738-e8jufleests3ede3irn3pckl5ofih5ja.apps.googleusercontent.com"
+  end
 end
